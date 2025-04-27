04b279c517095eb8e3811304ac97e7146247d7c6
Benjamin-Kabeya <bukasabenjamin6@gmail.com>
2025-04-27 18:50:03 +0100

    feat: add of AdvancedTable component

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,14 @@
 const express = require ('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-const server = express();
+const app = express();
 
 
-server.use(express.json());
+app.use(express.json());
 
 
 
 
 
-server.listen(PORT, console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, console.log(`Server is running on port ${PORT}`)
 );
